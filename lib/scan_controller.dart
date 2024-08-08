3207eab612fb07c21c60276cc3a37a8987496ba7
MSaeed <mahmoudsaeed1996@gmail.com>
2024-08-08 14:18:09 +0300

    Detect objects without a box

--- lib/scan_controller.dart
+++ lib/scan_controller.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:camera/camera.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -8,28 +11,61 @@ class ScanController extends GetxController {
   late List<CameraDescription> cameras;
 
   RxBool isCameraInitialized = false.obs;
+  int cameraCount = 0;
 
   @override
   void onInit() {
     super.onInit();
     initCamera();
-  }
-
-  @override
-  void onClose() {
-    cameraController.dispose();
-    super.onClose();
+    initTFlite();
   }
 
   void initCamera() async {
     if(await Permission.camera.request().isGranted){
       cameras = await availableCameras();
       cameraController = CameraController(cameras.first, ResolutionPreset.max);
-      await cameraController.initialize();
+      await cameraController.initialize().then((value) {
+        cameraController.startImageStream((image) {
+          cameraCount++;
+          if(cameraCount % 10 == 0){
+            cameraCount = 0;
+            objectDetector(image);
+          }
+          update();
+        });
+      });
       isCameraInitialized(true);
       update();
     } else {
       print("Permission denied");
     }
+  }
+
+  initTFlite() async {
+    await Tflite.loadModel(model: 'assets/model.tflite', labels: 'assets/labels.txt', isAsset: true, numThreads: 1, useGpuDelegate: false);
+  }
+
+  Future<void> objectDetector(CameraImage image) async {
+    List<dynamic>? detector =  await Tflite.runModelOnFrame(bytesList: image.planes.map((e) {
+      return e.bytes;
+    }).toList(),
+    asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+    if(detector != null){
+      log('Result is $detector');
+    }
+  }
+
+  @override
+  void onClose() {
+    cameraController.dispose();
+    super.onClose();
   }
 }
