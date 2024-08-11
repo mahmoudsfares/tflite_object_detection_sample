66ab7d7664ab0d52879e8879d8a6cabdbccfbbf5
MSaeed <mahmoudsaeed1996@gmail.com>
2024-08-11 13:40:39 +0300

    Surround detected objects with a box

--- lib/camera_view.dart
+++ lib/camera_view.dart
@@ -12,7 +12,31 @@ class CameraView extends StatelessWidget {
       body: GetBuilder<ScanController>(
         init: ScanController(),
         builder: (controller) {
-          return controller.isCameraInitialized.value ? CameraPreview(controller.cameraController) : const Text('Loading preview...');
+          return controller.isCameraInitialized.value ? Stack(
+            children: [
+              CameraPreview(controller.cameraController),
+              Positioned(
+                top: controller.y * 700,
+                right: controller.x * 500,
+                child: Container(
+                  width: controller.w * 100 * context.width / 100,
+                  height: controller.h * 100 * context.height / 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.green, width: 4.0),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                          color: Colors.white,
+                          child: Text(controller.label))
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ) : const Text('Loading preview...');
         },
       ),
     );
--- lib/scan_controller.dart
+++ lib/scan_controller.dart
@@ -1,33 +1,37 @@
-import 'dart:developer';
-
 import 'package:camera/camera.dart';
 import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class ScanController extends GetxController {
-
   late CameraController cameraController;
   late List<CameraDescription> cameras;
 
   RxBool isCameraInitialized = false.obs;
   int cameraCount = 0;
 
+  double x = 0.0;
+  double y = 0.0;
+  double w = 0.0;
+  double h = 0.0;
+
+  String label = '';
+
   @override
   void onInit() {
     super.onInit();
     initCamera();
-    initTFlite();
+    initTFLite();
   }
 
   void initCamera() async {
-    if(await Permission.camera.request().isGranted){
+    if (await Permission.camera.request().isGranted) {
       cameras = await availableCameras();
       cameraController = CameraController(cameras.first, ResolutionPreset.max);
       await cameraController.initialize().then((value) {
         cameraController.startImageStream((image) {
           cameraCount++;
-          if(cameraCount % 10 == 0){
+          if (cameraCount % 10 == 0) {
             cameraCount = 0;
             objectDetector(image);
           }
@@ -41,25 +45,35 @@ class ScanController extends GetxController {
     }
   }
 
-  initTFlite() async {
+  initTFLite() async {
     await Tflite.loadModel(model: 'assets/model.tflite', labels: 'assets/labels.txt', isAsset: true, numThreads: 1, useGpuDelegate: false);
   }
 
   Future<void> objectDetector(CameraImage image) async {
-    List<dynamic>? detector =  await Tflite.runModelOnFrame(bytesList: image.planes.map((e) {
-      return e.bytes;
-    }).toList(),
-    asynch: true,
+    var recognitions = await Tflite.detectObjectOnFrame(
+      bytesList: image.planes.map((plane) {
+        return plane.bytes;
+      }).toList(),
       imageHeight: image.height,
       imageWidth: image.width,
+      model: "SSDMobileNet",
       imageMean: 127.5,
       imageStd: 127.5,
-      numResults: 1,
       rotation: 90,
+      numResultsPerClass: 1,
       threshold: 0.4,
+      asynch: true,
     );
-    if(detector != null){
-      log('Result is $detector');
+    if (recognitions!.isNotEmpty) {
+      dynamic detectedObject = recognitions.first;
+      if (detectedObject['confidenceInClass'] * 100 > 45) {
+        label = detectedObject['detectedClass'].toString();
+        h = detectedObject['rect']['h'];
+        w = detectedObject['rect']['w'];
+        x = detectedObject['rect']['x'];
+        y = detectedObject['rect']['y'];
+        update();
+      }
     }
   }
 
